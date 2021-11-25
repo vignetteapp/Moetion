36e491f671789c7d883e0f3d355c40349191a0e4
Speykious <speykious@gmail.com>
2021-11-25 20:57:43 +0100

    Complete port of `CalcMouth`

--- Moetion/Face/FaceSolver.cs
+++ Moetion/Face/FaceSolver.cs
@@ -2,6 +2,7 @@
 // This file is part of Moetion.
 // Moetion is licensed under the Microsoft Reciprocal License. See LICENSE for details.
 
+using System;
 using Akihabara.Framework.Protobuf;
 using Moetion.Extensions;
 
@@ -20,7 +21,6 @@ namespace Moetion.Face
         public static Mouth CalcMouth(NormalizedLandmarkList list)
         {
             var landmarks = list.Landmark;
-            Mouth data = new();
 
             // Eye keypoints
             var eyeInnerCornerL = landmarks[133].ToVector();
@@ -43,12 +43,38 @@ namespace Moetion.Face
             var mouthWidth = mouthCornerLeft.Distance(mouthCornerRight);
 
             // Mouth open and mouth shape ratios
-            // var ratioXY = mouthWidth / mouthOpen;
             var ratioY = mouthOpen / eyeInnerDistance;
             var ratioX = mouthWidth / eyeOuterDistance;
 
+            // Normalize and scale mouth open
+            ratioY = ratioY.Remap(0.15, 0.7);
 
-            return data;
+            // Normalize and scale mouth shape
+            ratioX = ratioX.Remap(0.45, 0.9);
+            ratioX = (ratioX - 0.3) * 2;
+
+            var mouthX = ratioX;
+            var mouthY = (mouthOpen / eyeInnerDistance).Remap(0.17, 0.5);
+
+            var ratioI = Math.Clamp(mouthX.Remap(0, 1) * 2 * mouthY.Remap(0.2, 0.7), 0, 1);
+            var ratioA = mouthY * 0.4 + mouthY * (1 - ratioI) * 0.6;
+            var ratioU = mouthY * (1 - ratioI).Remap(0, 0.3) * 0.1;
+            var ratioE = ratioU.Remap(0.2, 1) * (1 - ratioI) * 0.3;
+            var ratioO = (1 - ratioI) * mouthY.Remap(0.3, 1) * 0.4;
+
+            return new Mouth
+            {
+                X = ratioX,
+                Y = ratioY,
+                Shape = new MouthShape
+                {
+                    A = ratioA,
+                    I = ratioI,
+                    U = ratioU,
+                    E = ratioE,
+                    O = ratioO,
+                },
+            };
         }
     }
 }
