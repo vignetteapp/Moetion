d69794061cb376cfef2ae8f857b5dd333d739f2e
Speykious <speykious@gmail.com>
2021-11-25 20:34:43 +0100

    Add `Mouth` and `MouthShape` structs

---
+++ Moetion/Face/Mouth.cs
@@ -0,0 +1,22 @@
+// Copyright (c) The Vignette Authors
+// This file is part of Moetion.
+// Moetion is licensed under the Microsoft Reciprocal License. See LICENSE for details.
+
+namespace Moetion.Face
+{
+    public struct Mouth
+    {
+        /// <summary>
+        /// Openness of the mouth horizontally.
+        /// </summary>
+        public float X;
+        /// <summary>
+        /// Openness of the mouth vertically.
+        /// </summary>
+        public float Y;
+        /// <summary>
+        /// The vowel shape that the mouth is doing.
+        /// </summary>
+        public MouthShape Shape;
+    }
+}
---
+++ Moetion/Face/MouthShape.cs
@@ -0,0 +1,33 @@
+// Copyright (c) The Vignette Authors
+// This file is part of Moetion.
+// Moetion is licensed under the Microsoft Reciprocal License. See LICENSE for details.
+
+namespace Moetion.Face
+{
+    /// <summary>
+    /// The vowel shape that a <see cref="Mouth"/> is doing.
+    /// </summary>
+    public struct MouthShape
+    {
+        /// <summary>
+        /// How much the mouth tends to an 'A' shape.
+        /// </summary>
+        public float A;
+        /// <summary>
+        /// How much the mouth tends to an 'I' shape.
+        /// </summary>
+        public float I;
+        /// <summary>
+        /// How much the mouth tends to an 'U' shape.
+        /// </summary>
+        public float U;
+        /// <summary>
+        /// How much the mouth tends to an 'E' shape.
+        /// </summary>
+        public float E;
+        /// <summary>
+        /// How much the mouth tends to an 'O' shape.
+        /// </summary>
+        public float O;
+    }
+}
