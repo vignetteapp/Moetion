58388b9683907f39af75fdbc911f83f491d565e6
Speykious <speykious@gmail.com>
2021-11-28 00:56:18 +0100

    Add `Eyes` struct

---
+++ Moetion/Face/Eyes.cs
@@ -0,0 +1,18 @@
+// Copyright (c) The Vignette Authors
+// This file is part of Moetion.
+// Moetion is licensed under the BSD 3-Clause License. See LICENSE for details.
+
+namespace Moetion.Face
+{
+    public struct Eyes
+    {
+        /// <summary>
+        /// Openness of the left eye.
+        /// </summary>
+        public float Left;
+        /// <summary>
+        /// Openness of the right eye.
+        /// </summary>
+        public float Right;
+    }
+}
