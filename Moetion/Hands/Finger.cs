206c478a477db3941f575289a071a6948f1cbe39
Ayane <ayane@vignetteapp.org>
2022-01-04 13:19:59 +0000

    Implement finger struct
    Thanks GitHub Copilot
    
    Signed-off-by: GitHub <noreply@github.com>

---
+++ Moetion/Hands/Finger.cs
@@ -0,0 +1,15 @@
+// Copyright (c) The Vignette Authors
+// This file is part of Moetion.
+// Moetion is licensed under the BSD 3-Clause License. See LICENSE for details.
+
+using System.Numerics;
+
+namespace Moetion.Hands
+{
+    public struct Finger
+    {
+        public Vector3 Proximal;
+        public Vector3 Intermediate;
+        public Vector3 Distal;
+    }
+}
--- Moetion/Hands/Hand.cs
+++ Moetion/Hands/Hand.cs
@@ -13,46 +13,12 @@ namespace Moetion.Hands
         public Vector3 Rotation;
         public Vector3 Wrist;
 
-        // TODO: make a Finger struct with Proximal, Intermediate and Distal fields. Will be much more readable and easy to work with, probably.
-
-        #region Thumb
-
-        public Vector3 ThumbProximal;
-        public Vector3 ThumbIntermediate;
-        public Vector3 ThumbDistal;
-
-        #endregion
-
-        #region Index Finger
-
-        public Vector3 IndexProximal;
-        public Vector3 IndexIntermediate;
-        public Vector3 IndexDistal;
-
-        #endregion
-
-        #region Middle Finger
-
-        public Vector3 MiddleProximal;
-        public Vector3 MiddleIntermediate;
-        public Vector3 MiddleDistal;
-
-        #endregion
-
-        #region Ring Finger
-
-        public Vector3 RingProximal;
-        public Vector3 RingIntermediate;
-        public Vector3 RingDistal;
-
-        #endregion
-
-        #region Little Finger
-
-        public Vector3 LittleProximal;
-        public Vector3 LittleIntermediate;
-        public Vector3 LittleDistal;
-
+        #region Fingers
+        public Finger Thumb;
+        public Finger Index;
+        public Finger Middle;
+        public Finger Ring;
+        public Finger Little;
         #endregion
     }
 }
