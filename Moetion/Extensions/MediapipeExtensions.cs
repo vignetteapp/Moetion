755bf217b5961f72873f59a73b02efbc3b003df4
Speykious <speykious@gmail.com>
2022-10-23 00:49:11 +0200

    Make `PoseSolver` also use world landmarks for solving (#16)
    * Copy `NormalizedLandmark` extensions for `Landmark`
    
    * Also use world landmarks for `PoseSolver`

--- Moetion/Extensions/MediapipeExtensions.cs
+++ Moetion/Extensions/MediapipeExtensions.cs
@@ -10,6 +10,7 @@ namespace Moetion.Extensions;
 
 public static class MediapipeExtensions
 {
+    #region NormalizedLandmark
     public static Vector3 ToVector(this NormalizedLandmark landmark) => new Vector3(landmark.X, landmark.Y, landmark.Z);
     public static Vector2 ToVector2(this NormalizedLandmark landmark) => new Vector2(landmark.X, landmark.Y);
 
@@ -21,4 +22,19 @@ public static class MediapipeExtensions
 
     public static float AngleBetweenLandmarks(this RepeatedField<NormalizedLandmark> lm, int a, int b, int c)
         => VectorExtensions.AngleBetween3DCoords(lm[a].ToVector(), lm[b].ToVector(), lm[c].ToVector());
+    #endregion
+
+    #region Landmark
+    public static Vector3 ToVector(this Landmark landmark) => new Vector3(landmark.X, landmark.Y, landmark.Z);
+    public static Vector2 ToVector2(this Landmark landmark) => new Vector2(landmark.X, landmark.Y);
+
+    public static Vector3 FindRotation(this Landmark landmark, Landmark other, bool normalize = true)
+        => landmark.ToVector().FindRotation(other.ToVector(), normalize);
+
+    public static Vector3 RollPitchYaw(this RepeatedField<Landmark> lm, int a, int b, int? oc = null)
+        => VectorExtensions.RollPitchYaw(lm[a].ToVector(), lm[b].ToVector(), oc is int c ? lm[c].ToVector() : null);
+
+    public static float AngleBetweenLandmarks(this RepeatedField<Landmark> lm, int a, int b, int c)
+        => VectorExtensions.AngleBetween3DCoords(lm[a].ToVector(), lm[b].ToVector(), lm[c].ToVector());
+    #endregion
 }
