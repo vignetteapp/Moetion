4ecc39cc4e908ed481e564894f6b4767ccade712
Speykious <speykious@gmail.com>
2022-10-22 17:18:46 +0200

    Also use world landmarks for `PoseSolver`

--- Moetion/Pose/PoseSolver.cs
+++ Moetion/Pose/PoseSolver.cs
@@ -14,21 +14,22 @@ namespace Moetion.Pose;
 
 public static class PoseSolver
 {
-    public static Pose Solve(NormalizedLandmarkList list, bool enableLegs = true)
+    public static Pose Solve(LandmarkList worldLandmarks, NormalizedLandmarkList normLandmarks, bool enableLegs = true)
     {
-        (Arm leftArm, Arm rightArm) = calcArms(list);
-        (Hips hips, Vector3 spine) = calcHips(list);
-        RepeatedField<NormalizedLandmark> landmarks = list.Landmark;
+        (Arm leftArm, Arm rightArm) = calcArms(worldLandmarks);
+        (Hips hips, Vector3 spine) = calcHips(worldLandmarks, normLandmarks);
+        RepeatedField<Landmark> wlm = worldLandmarks.Landmark;
+        RepeatedField<NormalizedLandmark> nlm = normLandmarks.Landmark;
 
         // Detect offscreen and reset values to defaults
-        bool rightHandOffscreen = landmarks[15].ToVector().Y > -.1f
-                                  || landmarks[15].Visibility < .23f
-                                  || .995f < landmarks[15].ToVector2().Y;
-        bool leftHandOffscreen = landmarks[16].ToVector().Y > -.1f
-                                 || landmarks[16].Visibility < .23f
-                                 || .995f < landmarks[16].ToVector2().Y;
-        bool leftFootOffscreen = landmarks[23].Visibility < .63f || hips.Position.Z > -.4f;
-        bool rightFootOffscreen = landmarks[24].Visibility < .63f || hips.Position.Z > -.4f;
+        bool rightHandOffscreen = nlm[15].ToVector().Y > -.1f
+                                  || nlm[15].Visibility < .23f
+                                  || .995f < nlm[15].ToVector2().Y;
+        bool leftHandOffscreen = nlm[16].ToVector().Y > -.1f
+                                 || nlm[16].Visibility < .23f
+                                 || .995f < nlm[16].ToVector2().Y;
+        bool leftFootOffscreen = nlm[23].Visibility < .63f || hips.Position.Z > -.4f;
+        bool rightFootOffscreen = nlm[24].Visibility < .63f || hips.Position.Z > -.4f;
 
         if (leftHandOffscreen)
         {
@@ -60,7 +61,7 @@ public static class PoseSolver
         // Skip calculations if disable legs
         if (enableLegs)
         {
-            (Leg leftLeg, Leg rightLeg) = calcLegs(list);
+            (Leg leftLeg, Leg rightLeg) = calcLegs(worldLandmarks);
             leftLeg.Upper *= rightFootOffscreen ? 0 : 1;
             leftLeg.Lower *= rightFootOffscreen ? 0 : 1;
             rightLeg.Upper *= leftFootOffscreen ? 0 : 1;
@@ -72,30 +73,30 @@ public static class PoseSolver
         return pose;
     }
 
-    private static (Arm, Arm) calcArms(NormalizedLandmarkList list)
+    private static (Arm, Arm) calcArms(LandmarkList worldLandmarks)
     {
-        RepeatedField<NormalizedLandmark> landmarks = list.Landmark;
+        RepeatedField<Landmark> wlm = worldLandmarks.Landmark;
 
         Arm rightArm = new Arm();
         Arm leftArm = new Arm();
 
-        rightArm.Upper = landmarks[11].FindRotation(landmarks[13]);
-        leftArm.Upper = landmarks[12].FindRotation(landmarks[14]);
-        rightArm.Upper.Y = landmarks.AngleBetweenLandmarks(12, 11, 13);
-        leftArm.Upper.Y = landmarks.AngleBetweenLandmarks(11, 12, 14);
+        rightArm.Upper = wlm[11].FindRotation(wlm[13]);
+        leftArm.Upper = wlm[12].FindRotation(wlm[14]);
+        rightArm.Upper.Y = wlm.AngleBetweenLandmarks(12, 11, 13);
+        leftArm.Upper.Y = wlm.AngleBetweenLandmarks(11, 12, 14);
 
-        rightArm.Lower = landmarks[13].FindRotation(landmarks[15]);
-        leftArm.Lower = landmarks[14].FindRotation(landmarks[16]);
-        rightArm.Lower.Y = landmarks.AngleBetweenLandmarks(11, 13, 15);
-        leftArm.Lower.Y = landmarks.AngleBetweenLandmarks(12, 14, 16);
+        rightArm.Lower = wlm[13].FindRotation(wlm[15]);
+        leftArm.Lower = wlm[14].FindRotation(wlm[16]);
+        rightArm.Lower.Y = wlm.AngleBetweenLandmarks(11, 13, 15);
+        leftArm.Lower.Y = wlm.AngleBetweenLandmarks(12, 14, 16);
         rightArm.Lower.Z = Math.Clamp(rightArm.Lower.Z, -2.14f, 0f);
         leftArm.Lower.Z = Math.Clamp(leftArm.Lower.Z, -2.14f, 0f);
 
-        rightArm.Hand = landmarks[15].ToVector().FindRotation(
-            Vector3.Lerp(landmarks[17].ToVector(), landmarks[19].ToVector(), .5f)
+        rightArm.Hand = wlm[15].ToVector().FindRotation(
+            Vector3.Lerp(wlm[17].ToVector(), wlm[19].ToVector(), .5f)
         );
-        leftArm.Hand = landmarks[16].ToVector().FindRotation(
-            Vector3.Lerp(landmarks[18].ToVector(), landmarks[20].ToVector(), .5f)
+        leftArm.Hand = wlm[16].ToVector().FindRotation(
+            Vector3.Lerp(wlm[18].ToVector(), wlm[20].ToVector(), .5f)
         );
 
         // Modify rotations slightly for more natural movement
@@ -129,15 +130,16 @@ public static class PoseSolver
         arm.Hand.Z = arm.Hand.Z * -2.3f * invert; // up down
     }
 
-    private static (Hips, Vector3) calcHips(NormalizedLandmarkList list)
+    private static (Hips, Vector3) calcHips(LandmarkList worldLandmarks, NormalizedLandmarkList normLandmarks)
     {
-        RepeatedField<NormalizedLandmark> landmarks = list.Landmark;
+        RepeatedField<Landmark> wlm = worldLandmarks.Landmark;
+        RepeatedField<NormalizedLandmark> nlm = normLandmarks.Landmark;
 
         // Find 2D normalized hip and shoulder joint positions / distances
-        Vector2 hipLeft2d = landmarks[23].ToVector2();
-        Vector2 hipRight2d = landmarks[24].ToVector2();
-        Vector2 shoulderLeft2d = landmarks[11].ToVector2();
-        Vector2 shoulderRight2d = landmarks[12].ToVector2();
+        Vector2 hipLeft2d = nlm[23].ToVector2();
+        Vector2 hipRight2d = nlm[24].ToVector2();
+        Vector2 shoulderLeft2d = nlm[11].ToVector2();
+        Vector2 shoulderRight2d = nlm[12].ToVector2();
 
         // ...Why is it 1 and not .5f?   ',:v
         Vector2 hipCenter2d = Vector2.Lerp(hipLeft2d, hipRight2d, 1);
@@ -153,7 +155,7 @@ public static class PoseSolver
                 Y = 0,
                 Z = Math.Clamp(spineLength - 1, -2, 0),
             },
-            Rotation = landmarks.RollPitchYaw(23, 24),
+            Rotation = wlm.RollPitchYaw(23, 24),
         };
 
         // Fix -PI, PI jumping
@@ -173,7 +175,7 @@ public static class PoseSolver
         hips.Rotation.Z *= 1 - turnAroundAmountHips;
         hips.Rotation.X = 0; // Temp fix for inaccurate X axis
 
-        Vector3 spine = landmarks.RollPitchYaw(11, 12);
+        Vector3 spine = wlm.RollPitchYaw(11, 12);
 
         // fix -PI, PI jumping
         if (spine.Y > .5f)
@@ -213,31 +215,31 @@ public static class PoseSolver
     }
 
     // NOTE: Legs are a WIP on the Kalidokit side.
-    private static (Leg, Leg) calcLegs(NormalizedLandmarkList list)
+    private static (Leg, Leg) calcLegs(LandmarkList worldLandmarks)
     {
-        RepeatedField<NormalizedLandmark> landmarks = list.Landmark;
+        RepeatedField<Landmark> wlm = worldLandmarks.Landmark;
 
         Leg leftLeg = new Leg
         {
-            Upper = landmarks[24].FindRotation(landmarks[26]),
-            Lower = landmarks[26].FindRotation(landmarks[28]),
+            Upper = wlm[24].FindRotation(wlm[26]),
+            Lower = wlm[26].FindRotation(wlm[28]),
         };
         Leg rightLeg = new Leg
         {
-            Upper = landmarks[23].FindRotation(landmarks[25]),
-            Lower = landmarks[25].FindRotation(landmarks[27]),
+            Upper = wlm[23].FindRotation(wlm[25]),
+            Lower = wlm[25].FindRotation(wlm[27]),
         };
 
         // Recenter
         leftLeg.Upper.Z = Math.Clamp(leftLeg.Upper.Z - .5f, -.5f, 0);
         leftLeg.Upper.Y = 0; // Y axis is not correct
-        leftLeg.Lower.X = landmarks.AngleBetweenLandmarks(24, 26, 28);
+        leftLeg.Lower.X = wlm.AngleBetweenLandmarks(24, 26, 28);
         leftLeg.Lower.Y = 0; // Y axis not correct
         leftLeg.Lower.Z = 0; // Z axis not correct
 
         rightLeg.Upper.Z = Math.Clamp(rightLeg.Upper.Z - .5f, -.5f, 0);
         rightLeg.Upper.Y = 0; // Y axis is not correct
-        rightLeg.Lower.X = landmarks.AngleBetweenLandmarks(23, 25, 27);
+        rightLeg.Lower.X = wlm.AngleBetweenLandmarks(23, 25, 27);
         rightLeg.Lower.Y = 0; // Y axis not correct
         rightLeg.Lower.Z = 0; // Z axis not correct
 
